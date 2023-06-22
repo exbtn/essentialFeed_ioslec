afb89e99adca35562f1f4aa25045fcd06926a0cc
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2023-06-22 13:25:07 +0200

    Fix Result type and handle it accordingly

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -80,8 +80,15 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
     
     private func save(_ feed: [FeedImage], with loader: LocalFeedLoader, file: StaticString = #filePath, line: UInt = #line) {
         let saveExp = expectation(description: "Wait for save completion")
-        loader.save(feed) { saveError in
-            XCTAssertNil(saveError, "Expected to save feed successfully", file: file, line: line)
+        loader.save(feed) { result in
+            switch result {
+            case .success:
+                break
+                
+            case let .failure(error):
+                XCTFail("Expected to save feed successfully, got \(error) instead", file: file, line: line)
+            }
+            
             saveExp.fulfill()
         }
         wait(for: [saveExp], timeout: 1)
