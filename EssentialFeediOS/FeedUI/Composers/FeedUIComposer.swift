17451358a6e6973c30420f8e94a35177dcba7413
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2023-08-27 18:43:04 +0200

    Add presentable view models as pure data to clarify communication between Presenter and UI

--- EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -40,8 +40,8 @@ private final class WeakRefVirtualProxy<T: AnyObject> {
 }
 
 extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
-    func display(isLoading: Bool) {
-        object?.display(isLoading: isLoading)
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
     }
 }
 
@@ -55,8 +55,8 @@ private final class FeedViewAdapter: FeedView {
         self.imageLoader = imageLoader
     }
     
-    func display(feed: [FeedImage]) {
-        controller?.tableModel = feed
+    func display(_ viewModel: FeedViewModel) {
+        controller?.tableModel = viewModel.feed
             .map { FeedImageViewModel(model: $0, imageLoader: imageLoader, imageTransformer: UIImage.init) }
             .map { FeedImageCellController(viewModel: $0) }
     }
