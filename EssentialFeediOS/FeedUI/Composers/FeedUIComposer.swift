f95b87087e5fbe01035893fecc8eed9347b05d4f
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2023-09-13 22:59:14 +0200

    Add return keyword to be more explicit

--- EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -56,7 +56,7 @@ extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
 
 extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        decoratee.loadImageData(from: url) { [weak self] result in
+        return decoratee.loadImageData(from: url) { [weak self] result in
             self?.dispatch { completion(result) }
         }
     }
