16d7cceb23d42f010944a9456004bea8f4b0169e
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2023-09-17 14:11:19 +0200

    `FeedPresenter` displays no error message on `didStartLoadingFeed`

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -7,9 +7,25 @@
 
 import XCTest
 
+struct FeedErorrViewModel {
+    let message: String?
+    
+    static var noError: FeedErorrViewModel { FeedErorrViewModel(message: .none) }
+}
+
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErorrViewModel)
+}
+
 final class FeedPresenter {
-    init(view: Any) {
-        
+    private let errorView: FeedErrorView
+    
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingFeed() {
+        errorView.display(.noError)
     }
 }
 
@@ -21,18 +37,34 @@ final class FeedPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
+    func test_didStartLoadingFeed_displaysNoErrorMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter(view: view)
+        let sut = FeedPresenter(errorView: view)
         trackForMemoryLeak(view, file: file, line: line)
         trackForMemoryLeak(sut, file: file, line: line)
         return (sut, view)
     }
     
-    private class ViewSpy {
-        let messages = [Any]()
+    private class ViewSpy: FeedErrorView {
+        enum Messages: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+        private(set) var messages = [Messages]()
+        
+        func display(_ viewModel: FeedErorrViewModel) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
     }
     
 }
