6621e5951dab4b9adfd4530694ba21357fc1c03c
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2024-02-11 22:59:23 +0100

    Move UITableViewController helpers to a separate file

---
+++ EssentialFeediOSTests/Helpers/UITableViewController+TestHelpers.swift
@@ -0,0 +1,50 @@
+//
+//  UITableViewController+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by Yevhenii Veretennikov on 11.02.2024.
+//
+
+import UIKit
+
+extension UITableViewController {
+    func simulateAppearance() {
+        if !isViewLoaded {
+            loadViewIfNeeded()
+            prepareForFirstAppearance()
+        }
+        
+        beginAppearanceTransition(true, animated: false)
+        endAppearanceTransition()
+    }
+    
+    private func prepareForFirstAppearance() {
+        replaceRefreshControlWithFakeForiOS17PlusSupport()
+    }
+    
+    private func replaceRefreshControlWithFakeForiOS17PlusSupport() {
+        let fakeRefreshControl = FakeUIRefreshControl()
+        
+        refreshControl?.allTargets.forEach { target in
+            refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { action in
+                fakeRefreshControl.addTarget(target, action: Selector(action), for: .valueChanged)
+            }
+        }
+        
+        refreshControl = fakeRefreshControl
+    }
+    
+    private class FakeUIRefreshControl: UIRefreshControl {
+        private var _isRefreshing = false
+        
+        override var isRefreshing: Bool { _isRefreshing }
+        
+        override func beginRefreshing() {
+            _isRefreshing = true
+        }
+        
+        override func endRefreshing() {
+            _isRefreshing = false
+        }
+    }
+}
