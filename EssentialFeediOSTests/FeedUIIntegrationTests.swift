47c2a60bd98a74b3eea07eb4a198fa8be5dd60e7
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2024-02-11 22:53:21 +0100

    Move UIButton helpers to a separate file

--- EssentialFeediOSTests/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/FeedUIIntegrationTests.swift
@@ -491,16 +491,6 @@ private extension FeedImageCell {
     }
 }
 
-private extension UIButton {
-    func simulateTap() {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
-    }
-}
-
 extension UITableViewController {
     func simulateAppearance() {
         if !isViewLoaded {
