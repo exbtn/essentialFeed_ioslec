f8471e04a009e0cc106874df5e9338b3305d96f4
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2024-02-11 22:55:04 +0100

    Move FeedImageCell helpers to a separate file

--- EssentialFeediOSTests/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/FeedUIIntegrationTests.swift
@@ -461,36 +461,6 @@ private extension FeedViewController {
     private var feedImageSection: Int { 0 }
 }
 
-private extension FeedImageCell {
-    var isShowingLocation: Bool {
-        return !locationContainer.isHidden
-    }
-    
-    var locationText: String? {
-        return locationLabel.text
-    }
-    
-    var descriptionText: String? {
-        return descriptionLabel.text
-    }
-    
-    var isShowingImageLoadingIndicator: Bool {
-        return feedImageContainer.isShimmering
-    }
-    
-    var renderedImage: Data? {
-        return feedImageView.image?.pngData()
-    }
-    
-    var isShowingRetryAction: Bool {
-        return !feedImageRetryButton.isHidden
-    }
-    
-    func simulateRetryAction() {
-        feedImageRetryButton.simulateTap()
-    }
-}
-
 extension UITableViewController {
     func simulateAppearance() {
         if !isViewLoaded {
---
+++ EssentialFeediOSTests/Helpers/FeedImageCell+TestHelpers.swift
@@ -0,0 +1,39 @@
+//
+//  FeedImageCell+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by Yevhenii Veretennikov on 11.02.2024.
+//
+
+import Foundation
+import EssentialFeediOS
+
+extension FeedImageCell {
+    var isShowingLocation: Bool {
+        return !locationContainer.isHidden
+    }
+    
+    var locationText: String? {
+        return locationLabel.text
+    }
+    
+    var descriptionText: String? {
+        return descriptionLabel.text
+    }
+    
+    var isShowingImageLoadingIndicator: Bool {
+        return feedImageContainer.isShimmering
+    }
+    
+    var renderedImage: Data? {
+        return feedImageView.image?.pngData()
+    }
+    
+    var isShowingRetryAction: Bool {
+        return !feedImageRetryButton.isHidden
+    }
+    
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
+}
